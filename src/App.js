4c89d4e5ad76a3738befd22d1f97850eadea8721
aslan9569 <1234aslan1234@mail.ru>
2021-03-28 20:20:03 +0300

    homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    const [text, setText] = useState('')
+
+    const [todos, setTodos] = useState([
+        {
+            text: 'первое дело',
+            done: true
+        },
+        {
+            text: 'не твое дело',
+            done: false
+        },
+        {
+            text: 'третье дело',
+            done: false
+        }
+    ])
+
+    const handleAdd = () => {
+        setTodos([
+            {
+                text: text
+            },
+            ...todos
+        ])
+    }
+
+    const handleChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const [check, setCheck] = useState("")
+    const checkClick = (event) => {
+        setCheck(event.target.checked)
+    }
+
+
+
+
+    // const handleDo = (event) => {
+    //     setCheck(event.target.value)
+    // }
+
+
+    return (
+        <div>
+            <div>
+                <input type="text" value={text} onChange={handleChange}/>
+                <button onClick={handleAdd}>
+                    add
+                </button>
+                <input type="checkbox" checked={check} onChange={checkClick} />
+
+            </div>
+            <ul>
+                {todos.map((item) => {
+                    return (
+                        <li>
+                            <input type="checkbox" checked={item.done} />
+                            {item.text}
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
